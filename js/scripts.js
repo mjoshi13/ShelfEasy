4a8a9e47a9b1ff82d2e51904a1f89f5c3ec48c20
mjoshi13 <48847775+mjoshi13@users.noreply.github.com>
2019-04-06 12:53:09 -0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+var i = 0;
+const images = document.querySelectorAll('img');
+const body = document.querySelector('body');
+const button = document.createElement("button");
+button.innerText = "Next image";
+
+
+
+for(let i = 0; i < images.length; i++) {
+    document.body.removeChild(images[i]);
+}
+
+
+
+function createDiv() {
+    let div = document.createElement('div');
+    div.appendChild(images[i]); 
+    return div;
+}
+
+let div = createDiv();
+body.appendChild(div);
+body.appendChild(button);
+
+
+
+button.addEventListener('click', () => {
+    i++;
+    if(i === images.length) {
+        i = 0;
+    }
+    body.removeChild(div);
+    div = createDiv();
+    body.insertBefore(div, button);
+});
